f48eaa2667cb2f2067abfd1a0173ed67189ad625
Yaarkoub <y.aarkoub@student.tudelft.nl>
2026-07-08 11:29:11 +0200

    deleted "bestanden vorige bap"

--- bestanden_vorige_BAP/SBC/transmission.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-## Run this script to deploy the SBC device client, after it has been registered.
-
-source "$WORK_DIR/.venv/bin/activate"
-
-python -m device.__run_dep_client__
--- bestanden_vorige_BAP/registration_software/tools/__init__.py
+++
@@ -1,5 +0,0 @@
-# src/tools/__init__.py
-from .calibrate import run_camera_calibration_tool
-
-
-__all__ = ["run_camera_calibration_tool"]
